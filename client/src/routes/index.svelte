bcd88ded319ba3e424350bf250b5526b0129e398
Cassidy Houlding <choulding@Cassidy-Houlding.local>
2020-01-29 19:43:43 -0500

    new homepage

--- client/src/routes/index.svelte
+++ client/src/routes/index.svelte
@@ -1,46 +1,40 @@
+<svelte:head>
+	<title>Events</title>
+</svelte:head>
+<script>
+ 	import Panel from "../components/Panel.svelte";
+	let events = [
+		{name: 'event1', date: 'April 1', src: 'great-success.png'},
+		{name: 'event2', date: 'June 2', src: 'favicon.png'},
+		{name: 'event3', date: 'June 14', src: 'logo-192.png'}
+		];
+</script>
 <style>
-	h1, figure, p {
-		text-align: center;
-		margin: 0 auto;
-	}
-
-	h1 {
-		font-size: 2.8em;
-		text-transform: uppercase;
-		font-weight: 700;
-		margin: 0 0 0.5em 0;
-	}
-
-	figure {
-		margin: 0 0 1em 0;
-	}
-
-	img {
-		width: 100%;
-		max-width: 400px;
-		margin: 0 0 1em 0;
-	}
-
+    .events {
+		display: block;
+    }
 	p {
-		margin: 1em auto;
+		vertical-align: top;
+		display: inline; 
+		position: absolute;
 	}
+	button {
+		display: inline;
+		vertical-align: center;
+		position: relative;
+		bottom: 100px;
+  		left: 800px;
 
-	@media (min-width: 480px) {
-		h1 {
-			font-size: 4em;
-		}
 	}
 </style>
-
-<svelte:head>
-	<title>Sapper project template</title>
-</svelte:head>
-
-<h1>Great success!</h1>
-
-<figure>
-	<img alt='Borat' src='great-success.png'>
-	<figcaption>HIGH FIVE!</figcaption>
-</figure>
-
-<p><strong>Try editing this file (src/routes/index.svelte) to test live reloading.</strong></p>
+<h1>Events</h1>
+<div class = "events">
+	{#each events as event}
+		<Panel title={event.name}>
+		<img alt='Event Photo' src={event.src} width="200" height="200">
+		<p>{event.name} <br> Date: {event.date}</p>
+		<button>Buy Tickets</button>
+		</Panel>
+		
+	{/each}
+</div>
