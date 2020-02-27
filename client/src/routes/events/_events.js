7b20cff00924da8470c15427621ca005a80d5f1c
Cassidy Houlding <choulding@Cassidy-Houlding.local>
2020-02-26 19:41:28 -0500

    sort button

--- client/src/routes/events/_events.js
+++ client/src/routes/events/_events.js
@@ -25,7 +25,7 @@ const events = [
     slug: 'event3',
     date: 'June 14',
     src: 'great-success.png',
-    artist: 'borat',
+    artist: 'Borat',
     description: `
     <p>First, you have to know what <a href='https://svelte.dev'>Svelte</a> is. Svelte is a UI framework with a bold new idea: rather than providing a library that you write code with (like React or Vue, for example), it's a compiler that turns your components into highly optimized vanilla JavaScript. If you haven't already read the <a href='https://svelte.dev/blog/frameworks-without-the-framework'>introductory blog post</a>, you should!</p>
 
--- client/src/routes/events/index.svelte
+++ client/src/routes/events/index.svelte
@@ -10,14 +10,23 @@
 <script>
 	export let events = [];
 	
-	function sortByArtist () {
-		//let temp =events[0];
-		//events[0] = events[1];
-		//events[1] = temp;	
-		
+	function sortByArtist () {	
 		for (let i = 0; i<events.length; i++) {
 			for (let j = i+1; j<events.length; j++) {
-				if (((events[i].artist).compareTo(events[j].artist))>0) {
+				if (events[i].artist > events[j].artist) {
+					let temp =events[i];
+					events[i] = events[j];
+					events[j] = temp;
+				}
+			}
+			
+		}
+		
+	}
+	function sortByName () {	
+		for (let i = 0; i<events.length; i++) {
+			for (let j = i+1; j<events.length; j++) {
+				if (events[i].name > events[j].name) {
 					let temp =events[i];
 					events[i] = events[j];
 					events[j] = temp;
@@ -59,29 +68,61 @@
 		color: rgba(64, 69, 237);
 	}
 	 .panel {
-    width: 100%;
-    border-radius: 10px;
-    border: 2px solid rgb(61, 61, 61);
-    overflow: hidden;
-    margin: 1rem 0.5rem;
-	position: relative;
+		width: 100%;
+		border-radius: 10px;
+		border: 2px solid rgb(61, 61, 61);
+		overflow: hidden;
+		margin: 1rem 0.5rem;
+		position: relative;
   }
-  .buttonwrapper {
-	position: absolute;
-	bottom: 45%;
-	right: 20px;
-    display: flex;
-    justify-content: flex-end;
-    flex-grow: 1;
-  }
-  .eventimg {
-	  float: left;
-	  clear: left;
-  }
+	.buttonwrapper {
+		position: absolute;
+		bottom: 45%;
+		right: 20px;
+		display: flex;
+		justify-content: flex-end;
+		flex-grow: 1;
+	}
+	.eventimg {
+		float: left;
+		clear: left;
+	}
 
+	.dropdown {
+		position: relative;
+		display: inline-block;
+	}
+	.dropdown-content {
+		display: none;
+		position: absolute;
+		background-color: #f1f1f1;
+		min-width: 160px;
+		box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
+		z-index: 1;
+	}
+	.options {
+		color: black;
+		background-color: #f1f1f1;
+		padding: 12px 16px;
+		min-width: 160px;
+		z-index: 1;
+		text-align: left
+	}
+
+	.dropdown:hover .dropdown-content {display: block;}
+	.dropdown-content button:hover {background-color: rgb(184, 184, 184);}
 </style>
+
 <h1 class="heading">EVENTS
-<button style ="position: absolute; right: 50px; top: 50px;" on:click={() =>sortByArtist()}>Sort</button>
+<div class= "dropdown">
+	<button class = "scrolldownbutton">Sort</button>
+	<div id = "myOptions" class="dropdown-content">
+            <button class=options on:click={() => sortByArtist()}>artist</button>
+            <button class=options on:click={() => sortByName()}>name</button>
+            <button class=options>something else</button>
+    </div>
+</div>
+
 </h1>
 <div class = "events">
 	{#each events as event}
