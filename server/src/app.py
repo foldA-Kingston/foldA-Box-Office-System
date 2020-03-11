3a36e2932dd855be49000a3ab172d1477d2965bb
Ross Hill <rosshill@protonmail.com>
2020-03-11 15:52:37 -0400

    Update api

--- server/src/app.py
+++ server/src/app.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
+from sqlathanor import FlaskBaseModel, initialize_flask_sqlathanor
 from flask_migrate import Migrate
 from flask_jwt_extended import (
     JWTManager, jwt_required, create_access_token,
@@ -7,7 +8,6 @@ from flask_jwt_extended import (
 )
 import enum
 import bcrypt
-import json
 
 app = Flask(__name__)
 
@@ -19,23 +19,16 @@ app.config['JWT_SECRET_KEY'] = 'super-secret'  # TODO: Change this
 jwt = JWTManager(app)
 
 SQLALCHEMY_TRACK_MODIFICATIONS = True
-db = SQLAlchemy(app)
+db = SQLAlchemy(app, model_class=FlaskBaseModel)
+db = initialize_flask_sqlathanor(db)
 
 migrate = Migrate(app, db)
 
 
-class Event_Ticket(db.Model):
-    __tablename__ = 'Event_Ticket'
-    id = db.Column(db.Integer, primary_key=True,
-                   autoincrement=True, nullable=False, unique=True)
-
-    event_id = db.Column(db.Integer, db.ForeignKey('Event.id'), nullable=False)
-    event = db.relationship('Event', backref='Event_Ticket')
-
-    ticket_id = db.Column(db.Integer, db.ForeignKey(
-        'Ticket.id'), nullable=False)
-    ticket = db.relationship(
-        'Ticket', backref='Event_Ticket')
+Event_Ticket = db.Table('Event_Ticket', db.MetaData(),
+    db.Column("event_id", db.Integer, db.ForeignKey('Event.id'), nullable=False),
+    db.Column("ticket_id", db.Integer, db.ForeignKey(
+        'Ticket.id'), nullable=False))
 
 
 class Event(db.Model):
@@ -55,7 +48,7 @@ class Event(db.Model):
     purchasable_id = db.Column(db.Integer, db.ForeignKey(
         'Purchasable.id'), nullable=False)
     purchasable = db.relationship(
-        'Purchasable', backref='Event')
+        'Purchasable', backref='Event', lazy="joined")
 
 
 class PurchasableTypes2(enum.Enum):
@@ -67,9 +60,17 @@ class Purchasable(db.Model):
     __tablename__ = 'Purchasable'
     id = db.Column(db.Integer, primary_key=True,
                    autoincrement=True, nullable=False, unique=True)
+
+    description = db.Column(db.String, nullable=False)
     type = db.Column(db.Enum(PurchasableTypes2), nullable=False)
     numTickets = db.Column(db.Integer, nullable=False)
     isSoldOut = db.Column(db.Boolean, nullable=False, default=False)
+
+    events = db.relationship(
+        'Event', backref='Purchasable', lazy="joined")
+
+    tickets = db.relationship(
+        'Ticket', backref='Purchasable', lazy="joined")
 
 
 class Ticket(db.Model):
@@ -95,20 +96,10 @@ class Ticket(db.Model):
     user = db.relationship('User', backref='Ticket')
 
 
-class Purchasable_TicketClass(db.Model):
-    __tablename__ = 'Purchasable_TicketClass'
-    id = db.Column(db.Integer, primary_key=True,
-                   autoincrement=True, nullable=False, unique=True)
-
-    purchasable_id = db.Column(db.Integer, db.ForeignKey(
-        'Purchasable.id'), nullable=False)
-    purchasable = db.relationship(
-        'Purchasable', backref='Purchasable_TicketClass')
-
-    ticketClass_id = db.Column(db.Integer, db.ForeignKey(
-        'TicketClass.id'), nullable=False)
-    ticketClass = db.relationship(
-        'TicketClass', backref='Purchasable_TicketClass')
+Purchasable_TicketClass = db.Table('Purchasable_TicketClass', db.MetaData(),
+                                   db.Column('purchasable_id', db.Integer, db.ForeignKey(
+                                       'Purchasable.id'), nullable=False),
+                                   db.Column('ticketClass_id',  db.Integer, db.ForeignKey('TicketClass.id'), nullable=False))
 
 
 class TicketClass(db.Model):
@@ -118,20 +109,31 @@ class TicketClass(db.Model):
     description = db.Column(db.String, nullable=False)
     price = db.Column(db.Integer, nullable=False)
 
+    tickets = db.relationship(
+        'Ticket', backref='TicketClass')
+
 
 class User(db.Model):
     __tablename__ = 'User'
     id = db.Column(db.Integer, primary_key=True,
-                   autoincrement=True, nullable=False, unique=True)
-    isAdmin = db.Column(db.Boolean, nullable=False, default=False)
-    emailAddress = db.Column(db.String, nullable=False, unique=True)
+                   autoincrement=True, nullable=False, unique=True, supports_json=True)
+    isAdmin = db.Column(db.Boolean, nullable=False,
+                        default=False, supports_json=True)
+    emailAddress = db.Column(db.String, nullable=False,
+                             unique=True, supports_json=True)
     createDate = db.Column(
-        db.DateTime, server_default=db.func.now(), nullable=False)
-    name = db.Column(db.String, nullable=False)
-    gender = db.Column(db.String)
-    birthDate = db.Column(db.Date)
-    association = db.Column(db.String)
+        db.DateTime, server_default=db.func.now(), nullable=False, supports_json=True)
+    name = db.Column(db.String, nullable=False, supports_json=True)
+    gender = db.Column(db.String, supports_json=True)
+    birthDate = db.Column(db.Date, supports_json=True)
+    association = db.Column(db.String, supports_json=True)
     password = db.Column(db.String, nullable=False)
+
+    feedbackAnswers = db.relationship(
+        'FeedbackAnswer', backref='User')
+    
+    tickets = db.relationship(
+        'Ticket', backref='User')
 
 
 class FeedbackAnswer(db.Model):
@@ -161,6 +163,9 @@ class FeedbackQuestion(db.Model):
         db.Integer, db.ForeignKey('Purchasable.id'), nullable=False)
     purchasable = db.relationship(
         'Purchasable', backref='FeedbackQuestion')
+
+    feedbackAnswers = db.relationship(
+        'FeedbackAnswer', backref='FeedbackQuestion')
 
 
 def serialize(obj):
@@ -194,7 +199,7 @@ def createUser():
     )
     db.session.add(user)
     db.session.commit()
-    user = db.session.query(User).filter(User.id == user.id).first()
+    user = db.session.query(User).filter(User.id == user.id).one()
     return serialize(user)
 
 
@@ -202,6 +207,7 @@ def createUser():
 @app.route("/users/", methods=['GET'])
 @jwt_required
 def getUsers():
+    identity = get_jwt_identity()
     if identity['isAdmin']:
         users = db.session.query(User).all()
         return jsonify([serialize(user) for user in users])
@@ -215,7 +221,7 @@ def getUser(id):
     identity = get_jwt_identity()
     id = int(id)
     if identity['id'] == id or identity['isAdmin']:
-        user = db.session.query(User).filter(User.id == id).first()
+        user = db.session.query(User).filter(User.id == id).one()
         return serialize(user)
     return "Forbidden", 403
 
@@ -227,7 +233,7 @@ def updateUser(id):
     identity = get_jwt_identity()
     id = int(id)
     if identity['id'] == id or identity['isAdmin']:
-        user = db.session.query(User).filter(User.id == id).first()
+        user = db.session.query(User).filter(User.id == id).one()
         user.name = request.json.get("name")
         db.session.commit()
         return serialize(user)
@@ -265,12 +271,17 @@ def createEvent():
 
         if request.json.get("purchasableId"):  # add to existing purchasable
             event.purchasable_id = request.json.get("purchasableId")
+            purchasable = db.session.query(Purchasable).filter(
+                Purchasable.id == event.purchasable_id).one()
+
+            # change individual to dayPass
+            purchasable.type = PurchasableTypes2.dayPass
 
         else:  # create event AND purchasable
             purchasable = Purchasable(
                 type=PurchasableTypes2(request.json['type']) if request.json.get(
                     'type') else PurchasableTypes2.individual,
-                numTickets=request.json.get("capacity")
+                numTickets=request.json.get("capacity"), description=request.json.get("description")
             )
             db.session.add(purchasable)
             db.session.flush()
@@ -278,25 +289,32 @@ def createEvent():
 
         db.session.add(event)
         db.session.commit()
-        event = db.session.query(Event).filter(Event.id == event.id).first()
+
+        event = db.session.query(Event).filter(Event.id == event.id).one()
         purchasable = db.session.query(Purchasable).filter(
-            Purchasable.id == purchasable.id).first()
-        return {'event': serialize(event), 'purchasable': serialize(purchasable)}
+            Purchasable.id == purchasable.id).one()
+        return {**serialize(event), "purchasable": serialize(purchasable)}
     return "Forbidden", 403
 
 
 # Get events
-@app.route("/events/", methods=['GET'])
-def getEvents():
-    events = db.session.query(Event).all()
-    return jsonify([serialize(event) for event in events])
+@app.route("/individualEvents/", methods=['GET'])
+def getIndividualEvents():
+    events = db.session.query(Event, Purchasable).filter(
+        Event.purchasable_id == Purchasable.id).filter(Purchasable.type == PurchasableTypes2.individual).all()
+
+    return jsonify([{**serialize(event), "purchasable": serialize(purchasable)} for (event, purchasable) in events])
 
 
 # Get one event
 @app.route("/events/<id>/", methods=['GET'])
 def getEvent(id):
-    event = db.session.query(Event).filter(Event.id == id).first()
-    return serialize(event)
+    event = db.session.query(Event).filter(Event.id == id).one()
+
+    # add related purchasable to event
+    purchasable = db.session.query(Purchasable).filter(
+        Purchasable.id == event.purchasable_id).one()
+    return {**serialize(event), "purchasable": serialize(purchasable)}
 
 
 # Update one event
@@ -306,7 +324,7 @@ def updateEvent(id):
     identity = get_jwt_identity()
     id = int(id)
     if identity['isAdmin']:
-        event = db.session.query(Event).filter(Event.id == id).first()
+        event = db.session.query(Event).filter(Event.id == id).one()
         event.artistName = request.json.get("artistName"),
         event.description = request.json.get("description"),
         event.startTime = request.json.get("startTime"),
@@ -331,6 +349,71 @@ def deleteEvent(id):
     return "Forbidden", 403
 
 
+# Create new purchasable
+@app.route("/purchasables/", methods=['POST'])
+@jwt_required
+def createDayPass():
+    identity = get_jwt_identity()
+    if identity['isAdmin']:
+        purchasable = Purchasable(
+            type=request.json.get("type"),
+            numTickets=request.json.get("numTickets"),
+            description=request.json.get("description")
+        )
+        db.session.add(purchasable)
+        db.session.commit()
+        purchasable = db.session.query(Purchasable).filter(
+            Purchasable.id == purchasable.id).one()
+        return serialize(purchasable)
+    return "Forbidden", 403
+
+
+# Get day passes
+@app.route("/dayPasses/", methods=['GET'])
+def getDayPasses():
+    purchasables = db.session.query(Purchasable).filter(Purchasable.type == PurchasableTypes2.dayPass).all()
+    return jsonify([{**serialize(p), "events": [serialize(e) for e in p.events]} for p in purchasables])
+
+
+# Get one purchasable
+@app.route("/purchasables/<id>/", methods=['GET'])
+def getPurchasable(id):
+    purchasable = db.session.query(Purchasable).filter(
+        Purchasable.id == id).one()
+    return {**serialize(purchasable), "events": [serialize(e) for e in purchasable.events]}
+
+
+# Update one purchasable
+@app.route("/purchasables/<id>/", methods=['PUT'])
+@jwt_required
+def updatePurchasable(id):
+    identity = get_jwt_identity()
+    id = int(id)
+    if identity['isAdmin']:
+        purchasable = db.session.query(Purchasable).filter(
+            Purchasable.id == id).one()
+        purchasable.type = request.json.get("type")
+        purchasable.numTickets = request.json.get("numTickets")
+        purchasable.description = request.json.get("description")
+        db.session.commit()
+        return serialize(purchasable)
+    return "Forbidden", 403
+
+
+# Delete one purchasable
+@app.route("/purchasables/<id>/", methods=['DELETE'])
+@jwt_required
+def deletePurchasable(id):
+    identity = get_jwt_identity()
+    id = int(id)
+    if identity['isAdmin']:
+        purchasable = db.session.query(Purchasable).filter(
+            Purchasable.id == id).delete()
+        db.session.commit()
+        return "Deleted purchasable {}".format(id)
+    return "Forbidden", 403
+
+
 @app.route('/auth/', methods=['POST'])
 def authenticate():
     if not request.is_json:
@@ -345,7 +428,7 @@ def authenticate():
         return jsonify({"msg": "Missing password parameter"}), 400
 
     user = db.session.query(User).filter(
-        User.emailAddress == emailAddress).first()
+        User.emailAddress == emailAddress).one()
 
     if checkPassword(password, user.password):
         # Identity can be any data that is json serializable
