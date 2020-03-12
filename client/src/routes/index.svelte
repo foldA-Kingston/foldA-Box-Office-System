537752696941574b915cafb113dcd6656210c5ef
Ross Hill <rosshill@protonmail.com>
2020-03-11 20:35:20 -0400

    Fetch events from backend

--- client/src/routes/index.svelte
+++ client/src/routes/index.svelte
@@ -1,48 +1,73 @@
-<svelte:head>
-	<title>Events</title>
-</svelte:head>
 <script>
-	 import Panel from "../components/Panel.svelte";
-	 
-	let events = [
-		{name: 'HowlRound Digital + Performance Convening', date: 'April 1', src: 'event1.png'},
-		{name: 'Choir!Choir!Choir!', date: 'June 2', src: 'event2.png'},
-		{name: 'event3', date: 'June 14', src: 'great-success.png'}
-		];
-		
-</script>
-<style>
-    .events {
-		display: block;
-    }
-	.heading {
-		font-family: "Verdana", sans-serif;
-		font-size: 40px;
-		color: rgba(64, 69, 237);
-	}
-	p {
-		vertical-align: top;
-		display: inline; 
-		position: absolute;
-	}
-	button {
-		display: inline;
-		vertical-align: center;
-		position: relative;
-		bottom: 100px;
-  		left: 800px;
+  import { onMount } from "svelte";
+  import Panel from "../components/Panel.svelte";
 
-	}
+  let events = [];
+
+  onMount(async () => {
+    const result = await fetch("http://localhost:5000/individualEvents/", {
+      mode: "cors",
+      headers: {
+        "Content-Type": "application/json"
+      }
+    });
+    events = await result.json();
+  });
+</script>
+
+<style>
+  .events {
+    display: block;
+  }
+  .heading {
+    font-family: "Verdana", sans-serif;
+    font-size: 40px;
+    color: rgba(64, 69, 237);
+  }
+
+  .panelContent {
+    display: flex;
+  }
+
+  .panelContent > * {
+    padding: 0.5rem;
+  }
+
+  .imageWrapper {
+    overflow: hidden;
+    border-radius: 50%;
+    height: 10rem;
+    width: 10rem;
+    padding: 0;
+    margin: 0;
+  }
+  .imageWrapper img {
+    height: 100%;
+    width: 100%;
+    display: block;
+  }
 </style>
-<h1 class=heading>EVENTS</h1>
-<div class = "events">
-	{#each events as event}
-		<Panel title={event.name}>
-		<img alt='Event Photo' src={event.src} width="200" height="200">
-		<p>{event.name} <br> Date: {event.date}</p>
-		<a href="Event">{event.name}</a>
-		<button>Buy Tickets</button>
-		</Panel>
-		
-	{/each}
+
+<svelte:head>
+  <title>Events</title>
+</svelte:head>
+<h1 class="heading">EVENTS</h1>
+<div class="events">
+  {#each events as event}
+    <Panel title={event.description}>
+      <div class="panelContent">
+        <div class="imageWrapper">
+          <img src="event2.png" alt={event.description} />
+        </div>
+        <div>
+          <div>{event.description} &bull; {event.startDate}</div>
+          <div class="artist">{event.artist}</div>
+          <div>Longer description...</div>
+        </div>
+        <div>
+          <button>Buy tickets</button>
+        </div>
+      </div>
+    </Panel>
+  {/each}
 </div>
