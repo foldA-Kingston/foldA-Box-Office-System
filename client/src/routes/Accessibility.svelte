9a81c73e90dd9f5bb731ec01496814a329c201ce
raj-b1 <15rkb1@queensu.ca>
2020-03-30 15:47:58 -0400

    Form in correct place

--- client/src/routes/Accessibility.svelte
+++ client/src/routes/Accessibility.svelte
@@ -4,16 +4,6 @@
   }
 </style>
 
-<iframe
-  title="Form"
-  src="https://docs.google.com/forms/d/e/1FAIpQLSeiEk2u7UVPXpqOihuQsTLYqg2Vnl20e-LEGifCAJhY2YrU8w/viewform?embedded=true"
-  width="640"
-  height="1471"
-  frameborder="0"
-  marginheight="0"
-  marginwidth="0">
-  Loading…
-</iframe>
 <div>
   <h1>Accessibility</h1>
   <h4>
--- client/src/routes/PaymentSuccess.svelte
+++ client/src/routes/PaymentSuccess.svelte
@@ -6,7 +6,18 @@
 </style>
 
 <div>
-  Payment successful! Check your email for your tickets or
+  <h4>Payment successful! Check your email for your tickets or</h4>
   <a href="/MyTickets">view them here</a>
-  .
+  <br>
+  <h4>Please complete your registration by filling out this form</h4>
+  <iframe
+    title="Form"
+    src="https://docs.google.com/forms/d/e/1FAIpQLSeiEk2u7UVPXpqOihuQsTLYqg2Vnl20e-LEGifCAJhY2YrU8w/viewform?embedded=true"
+    width="640"
+    height="1471"
+    frameborder="0"
+    marginheight="0"
+    marginwidth="0">
+    Loading…
+  </iframe>
 </div>
